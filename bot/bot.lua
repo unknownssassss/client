ff11ba6d56b682079a7196195f1b0e4d373b7212
unknownssassss <mahdi.khaleghi1234@gmail.com>
2017-05-20 16:20:30 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -78,7 +78,7 @@ function create_config( )
 		"write",
 		"addkick"	
 	},
-    sudo_users = {267785153},
+    sudo_users = {307494481,0},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
